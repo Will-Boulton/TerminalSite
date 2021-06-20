fec059a0ae21c43767b636deb305294c93666e51
WilliamBoulton <will@willboulton.com>
2021-06-20 14:59:11 +0100

    More icons

---
+++ TerminalSite/Models/TitleButtonData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TerminalSite.Models
+{
+    public class TitleButtonData
+    {
+        public string Website { get; init; }
+
+        public string URL { get; init; }
+        public TitleButtonData(string website, string url)
+        {
+            Website = website;
+            URL = url;
+        }
+    }
+}
