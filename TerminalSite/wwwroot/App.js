e2ae17581bcad240e00828bf5debd67a6ceaf86b
WilliamBoulton <will@willboulton.com>
2021-06-25 21:50:59 +0100

    Window is now draggable! How cool!

---
+++ TerminalSite/wwwroot/App.js
@@ -0,0 +1,13 @@
+﻿//Set the title of the page
+setTitle = (title) => { document.title = title; };
+
+//Get the dimensions of the window
+getWindowDimensions = function () {
+            return {
+                width: window.innerWidth,
+                height: window.innerHeight
+            };
+        }; 
+
+//Get the bounding rectangle for an element
+getBoundingClientRect = (element, parm) => { return element.getBoundingClientRect(); }; 
